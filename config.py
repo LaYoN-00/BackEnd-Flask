f919449b0ba96b7a96ed646858059c62c6cb2e34
LaYoN-00 <diego.mercado1066@alumnos.udg.mx>
2024-11-01 14:54:03 -0600

    Actualizacion del 01/11/2024

--- config.py
+++ config.py
@@ -4,12 +4,6 @@ class DevelopmentConfig():
     MYSQL_USER='root'
     MYSQL_PASSWORD=''
     MYSQL_DB='proyecto1-sis'
-    """
-    MYSQL_HOST='bztat8upqfndpd6qucna-mysql.services.clever-cloud.com'
-    MYSQL_USER='unujudqiaxyqxhsm'
-    MYSQL_PASSWORD='MBzKs25PmYglL5ysTs7P'
-    MYSQL_DB='bztat8upqfndpd6qucna'
-    """
 config={
     'development':DevelopmentConfig
 }
